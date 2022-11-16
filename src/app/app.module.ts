6e24044cf003d6844b375ca8a3a9c020563bc4db
Gabril <gjperes@ufpr.br>
2022-11-16 15:39:43 -0300

    Menu melhorado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { SharedModule } from './shared';
     EstadoModule,
     CidadeModule,
     EnderecoModule,
-    SharedModule
+    SharedModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent],
