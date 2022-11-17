b2268dae1f603f7e5406cde13ca4d35da99bac62
Gabril <gjperes@ufpr.br>
2022-11-17 11:24:53 -0300

    Chamada de login para o Web Service em Spring

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -1,18 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Login, Usuario } from 'src/app/shared';
 
-const LS_CHAVE = "usuarioLogado";
+const LS_CHAVE = 'usuarioLogado';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor() {}
+  BASE_URL = "http://localhost:8080/login/";
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
 
   public get usuarioLogado(): Usuario {
     const usu = localStorage[LS_CHAVE];
-    return (usu ? JSON.parse(localStorage[LS_CHAVE]) : null);
+    return usu ? JSON.parse(localStorage[LS_CHAVE]) : null;
   }
 
   public set usuarioLogado(usuario: Usuario) {
@@ -20,23 +28,7 @@ export class LoginService {
   }
 
   login(login: Login): Observable<Usuario | null> {
-    const func = new Usuario(1, "Func", login.login, login.senha, "FUNC");
-    const admin = new Usuario(1, "Admin", login.login, login.senha, "ADMIN");
-    const gerente = new Usuario(1, "Gerente", login.login, login.senha, "GERENTE");
-
-    let user = func;
-
-    if (login.login === login.senha) {
-      if (login.login === 'admin') {
-        user = admin;
-      } else if (login.login === 'gerente') {
-        user = gerente;
-      }
-
-      return of(user);
-    } else {
-      return of(null);
-    }
+    return this.httpClient.post<Usuario>(this.BASE_URL, login, this.httpOptions);
   }
 
   logout() {
